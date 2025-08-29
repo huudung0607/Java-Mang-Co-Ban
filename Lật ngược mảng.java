874b2ee41aa51fabf74a1e4ae86dfcb4f72288ef
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-29 17:24:32 +0700

    Create Lật ngược mảng.java

---
+++ Lật ngược mảng.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for(int i = 0; i < n ;i++){
+            list.add(sc.nextInt());
+        }
+        Collections.reverse(list);
+        for(int i : list){
+            System.out.print(i + " ");
+        }
+    }
+}
