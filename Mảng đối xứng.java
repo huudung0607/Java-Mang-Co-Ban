f8bfb7eab2af3b46e7716cd21c97b066cbe0428b
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-29 17:14:25 +0700

    Create Mảng đối xứng.java

---
+++ Mảng đối xứng.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0; i < n ;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0 ;i < n ;i++){
+            if(a[i] != a[n - i - 1]){
+                System.out.print("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+}
